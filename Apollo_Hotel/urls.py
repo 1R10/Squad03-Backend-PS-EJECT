806d152c37a9fcc58ad0f1d0b104aa4a3510c3e0
Emanuel João <163889723+Emanuel0505@users.noreply.github.com>
2026-04-09 21:31:04 -0300

    test

--- Apollo_Hotel/urls.py
+++ Apollo_Hotel/urls.py
@@ -21,7 +21,11 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('blog/', include('BlogPage.urls')),
+
+
+    
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
